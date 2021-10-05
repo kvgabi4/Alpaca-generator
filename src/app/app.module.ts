3c43c00de1982fc5f8f382e8d9a06895b3ca07ab
kvgabi4 <kvgabi4@gmail.com>
2021-10-05 03:37:51 +0200

    images

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PictureComponent } from './components/picture/picture.component';
+import { StyleComponent } from './components/style/style.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PictureComponent,
+    StyleComponent
   ],
   imports: [
     BrowserModule,
