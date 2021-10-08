0978635ec724f2b8167d11991e460c8501d6b026
kvgabi4 <kvgabi4@gmail.com>
2021-10-08 12:29:27 +0200

    randomize

--- src/app/components/picture/picture.component.ts
+++ src/app/components/picture/picture.component.ts
@@ -9,22 +9,21 @@ import { Alpaca } from 'src/app/model/alpaca';
 })
 export class PictureComponent implements OnInit {
 
-  background: string = '../assets/alpaca/backgrounds/blue50.png'
-  picture: Alpaca = {
-    accessories: [],
-    backgrounds: [],
-    ears: [],
-    eyes: [],
-    hair: [],
-    leg: [],
-    mouth: [],
-    neck: [],
-    nose: []};
-  // parts: string[] = Object.keys(this.picture);
   parts: string[] = [
     'backgrounds', 'neck', 'ears', 'nose',
     'leg', 'hair', 'accessories', 'eyes', 'mouth'];
 
+  alpaca: Alpaca = {
+    accessories: ['earings', 'flower', 'glasses', 'headphone'],
+    backgrounds: ['blue50', 'blue60', 'blue70', 'darkblue30', 'darkblue50', 'darkblue70', 'green50', 'green60', 'green70', 'grey40', 'grey70', 'grey80', 'red50', 'red60', 'red70', 'yellow50', 'yellow60', 'yellow70'],
+    ears: ['default', 'tilt-backward', 'tilt-forward'],
+    eyes: ['angry', 'default', 'naughty', 'panda', 'smart', 'star'],
+    hair: ['bang', 'curls', 'default', 'elegant', 'fancy', 'quiff', 'short'],
+    leg: ['bubble-tea', 'cookie', 'default', 'game-console', 'tilt-backward', 'tilt-forward'],
+    mouth: ['astonished', 'default', 'eating', 'laugh', 'tongue'],
+    neck: ['bend-backward', 'bend-forward', 'default', 'thick'],
+    nose: ['nose'],
+  }
   @Input() part: string = '';
   @Input() style: string = '';
 
@@ -53,6 +52,15 @@ export class PictureComponent implements OnInit {
     this.style = event;
     const index = this.parts.indexOf(this.part)
     this.source[index] = `../assets/alpaca/${this.part}/${this.style}.png`
+  }
+
+  randomize() {
+    for (let key in this.alpaca) {
+      const randomIndex = Math.floor(Math.random() * (this.alpaca[key].length - 1));
+      this.style = this.alpaca[key][randomIndex];
+      this.part = key;
+      this.styleChange(this.style);
+    }
   }
 
 }
--- src/app/components/style/style.component.ts
+++ src/app/components/style/style.component.ts
@@ -8,7 +8,7 @@ import { Alpaca } from 'src/app/model/alpaca';
 })
 export class StyleComponent implements OnInit {
 
-  alpaca: Alpaca = {
+  @Output() alpaca: Alpaca = {
     accessories: ['earings', 'flower', 'glasses', 'headphone'],
     backgrounds: ['blue50', 'blue60', 'blue70', 'darkblue30', 'darkblue50', 'darkblue70', 'green50', 'green60', 'green70', 'grey40', 'grey70', 'grey80', 'red50', 'red60', 'red70', 'yellow50', 'yellow60', 'yellow70'],
     ears: ['default', 'tilt-backward', 'tilt-forward'],
